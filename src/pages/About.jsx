ec24b5d1567a00a872dc49cb69e6d5af68eeaed7
CarlaSlattery <cslatterydev@gmail.com>
2023-04-15 15:27:15 +0100

    added pages and routing to app

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A react app to search Github profiles and see profile details. This
+        project is part of the
+        <a href="https://www.udemy.com/course/modern-react-front-to-back/">
+          {" "}
+          React Front To Back
+        </a>{" "}
+        Udemy course by
+        <strong>
+          <a href="https://traversymedia.com"> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+    </>
+  );
+}
+
+export default About
